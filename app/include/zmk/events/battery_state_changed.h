55af4f2088f4e61ed1c2ef606a53b66590d53477
Jaap Versteegh <j.r.versteegh@gmail.com>
2025-12-07 01:54:43 +0100

    Report battery millivolts

--- app/include/zmk/events/battery_state_changed.h
+++ app/include/zmk/events/battery_state_changed.h
@@ -10,16 +10,16 @@
 #include <zmk/event_manager.h>
 
 struct zmk_battery_state_changed {
-    // TODO: Other battery channels
     uint8_t state_of_charge;
+    uint16_t millivolts;
 };
 
 ZMK_EVENT_DECLARE(zmk_battery_state_changed);
 
 struct zmk_peripheral_battery_state_changed {
     uint8_t source;
-    // TODO: Other battery channels
     uint8_t state_of_charge;
+    uint16_t millivolts;
 };
 
 ZMK_EVENT_DECLARE(zmk_peripheral_battery_state_changed);
